ce238480d74a7347f24dfda490cb9d3548daa5f9
plehegar <plh@w3.org>
2020-12-08 14:01:14 -0500

    labs.w3.org config

--- pm2.config.js
+++ pm2.config.js
@@ -8,10 +8,11 @@ module.exports = {
       name      : 'normative-references',
       script    : 'serve.js',
       env: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        PORT    : 5000
       },
       error_file : "/var/log/nodejs/normative-references.err",
-      out_file : "/var/log/nodejs/normative-references.log",
+      out_file   : "/var/log/nodejs/normative-references.log",
       "node_args": "--max_old_space_size=400"
     }
   ]
